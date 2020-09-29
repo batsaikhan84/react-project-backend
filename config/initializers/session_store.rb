cb46ffc3e5478bc0d136156bfe684bd3cc5651df
Batsaikhan Ulambayar <Batsaikhan@Sugarmaas-MacBook-Pro.local>
2020-09-29 01:02:50 -0500

    initial commit

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env == "production"
-  Rails.application.config.session_store :cookie_store, key: "_backend", domain: "jdh-authentication-app-api.herokuapp.com"
+  Rails.application.config.session_store :cookie_store, key: "_backend", domain: "https://react-project-backend.herokuapp.com/"
 else
   Rails.application.config.session_store :cookie_store, key: "_backend"
 end
