182bff6cc02eef3d4ec863d16899b0991a3f20a7
Batsaikhan Ulambayar <Batsaikhan@Sugarmaas-MacBook-Pro.local>
2020-09-29 01:06:25 -0500

    session store key change to app name

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env == "production"
-  Rails.application.config.session_store :cookie_store, key: "_backend", domain: "https://react-project-backend.herokuapp.com/"
+  Rails.application.config.session_store :cookie_store, key: "_react-project-backend", domain: "https://react-project-backend.herokuapp.com/"
 else
-  Rails.application.config.session_store :cookie_store, key: "_backend"
+  Rails.application.config.session_store :cookie_store, key: "_react-project-backend"
 end
